8eba0563c03c7d2fcdfed940a1b03eb7b0c6ecec
Patrick Nilan <nilan.patrick@gmail.com>
2023-08-02 14:14:14 -0700

    Updated conversations

--- features/Messages/Conversations.js
+++ features/Messages/Conversations.js
@@ -2,48 +2,65 @@ import React, { Component, useState, useEffect, useRef } from 'react';
 import { View, Text, StyleSheet, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 import { format, render, cancel, register } from 'timeago.js';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Messages from './Messages';
 
 // const Stack = createNativeStackNavigator();
 
-const Conversations = () => {
+const Conversations = ({ currentUser }) => {
 
-  const currentUser = { idToken: 'hello' };
+  currentUser = { username: 'Patrick', idToken: 'hello' };
 
-  const [ conversationsArray, setConversationsArray ] = useState([]);
+  const [conversationsArray, setConversationsArray] = useState([]);
+  const [conversation, setConversation] = useState({});
 
-  useEffect(() => {
-    fetchConversations(currentUser.idToken);
-  }, []);
+  // useEffect(() => {
+  //   fetchConversations(currentUser.idToken);
+  // }, []);
 
-  const fetchConversations = (idToken) => {
-    axios.get('/api/converstaions/', {
-      headers: {
-        'authorization': idToken
-      }
-    })
-      .then((result) => {
-        setConversationsArray(result);
-      })
-      .catch((err) => {
-        console.error('Error fetching messages:', err);
-        setConversationsArray([]);
-      });
-  };
+  // const fetchConversations = (idToken) => {i
+  //   axios.get('/api/converstaions/', {
+  //     headers: {
+  //       'authorization': idToken
+  //     }
+  //   })
+  //     .then((result) => {
+  //       setConversationsArray(result);
+  //     })
+  //     .catch((err) => {
+  //       console.error('Error fetching messages:', err);
+  //       setConversationsArray([]);
+  //     });
+  // };
+
+
 
   const conversations = [
-    { id: 1, content: 'This is a message' },
-    { id: 2, content: 'This is another message This is another message This is another message This is another message This is another message ' }
+    { conversation_id: 1, user_1_id: 1, user_2_id: 2, user_1_username: 'Patrick', user_2_username: 'Biderman', content: 'This is a message', created_at: '2023-08-01 14:00:00' },
+    { conversation_id: 1, user_1_id: 1, user_2_id: 3, user_1_username: 'Patrick', user_2_username: 'Oatman', content: 'This is a another message', created_at: '2023-08-01 13:00:00' },
+    { conversation_id: 1, user_1_id: 4, user_2_id: 1, user_1_username: 'Slark Kent', user_2_username: 'Patrick', content: 'This is a much longer message', created_at: '2023-08-01 12:00:00' },
+    { conversation_id: 1, user_1_id: 1, user_2_id: 5, user_1_username: 'Patrick', user_2_username: 'Neen Goblin', content: 'Something something something', created_at: '2023-08-01 11:00:00' },
+    { conversation_id: 1, user_1_id: 6, user_2_id: 1, user_1_username: 'Illmatic', user_2_username: 'Patrick', content: 'Can I buy comic?', created_at: '2023-08-01 10:00:00' },
+    { conversation_id: 1, user_1_id: 6, user_2_id: 1, user_1_username: 'AmI', user_2_username: 'Patrick', content: 'Can I buy Spiderman comic?', created_at: '2023-08-01 10:00:00' },
   ];
 
   const renderConversation = ({ item }) => {
-    let timeAgo = format('2022-01-01T10:10:45Z', 'en_US');
+
+
+    const getRandomInt = (min, max) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    let bgColors = ['reddish', 'reddish', 'reddish', 'orange', 'orange', 'grey', 'lightgrey'];
+
+    let color = bgColors[getRandomInt(0, 6)];
 
     return (
-      <TouchableOpacity style={styles.conversationContainer}>
-        <View style={styles.iconContainer}>
+      <TouchableOpacity style={styles.conversationContainer} onPress={() => setConversation(item)}>
+        <View style={styles.iconContainer && styles[color]}>
           <Text style={styles.icon}>
-            P
+            { item.user_1_username !== currentUser.username ? item.user_1_username.slice(0, 1) : item.user_2_username.slice(0, 1)}
           </Text>
         </View>
         <View style={styles.messageContent}>
@@ -54,7 +71,7 @@ const Conversations = () => {
           </View>
           <View style={styles.elapsedTimeContainer}>
             <Text style={styles.elapsedTimeText}>
-              {timeAgo}
+              {format(item.created_at)}
             </Text>
           </View>
         </View>
@@ -62,20 +79,29 @@ const Conversations = () => {
     );
   };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Messages</Text>
-      <View style={styles.horizontalRule} />
-      <View style={styles.conversationsContainer}>
-        <FlatList
-          data={conversations}
-          renderItem={renderConversation}
-          keyExtractor={(item) => item.id}
-          showsVerticalScrollIndicator={false}
-        />
+  if (!conversation.conversation_id) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Messages</Text>
+        <View style={styles.horizontalRule} />
+        <View style={styles.conversationsContainer}>
+          <FlatList
+            data={conversations}
+            renderItem={renderConversation}
+            keyExtractor={(item, index) => index}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
       </View>
-    </View>
-  );
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Messages currentUser={currentUser} conversation={conversation} setConversation={setConversation} />
+      </View>
+    );
+  }
+
 };
 
 const styles = StyleSheet.create({
@@ -105,9 +131,33 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   iconContainer: {
+
+  },
+  reddish: {
+    backgroundColor: "#EF6461",
     width: 50,
     height: 50,
-    backgroundColor: '#EF6461',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  grey: {
+    backgroundColor: "#E0DFD5",
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  orange: {
+    backgroundColor: "#E4B363",
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  lightgrey: {
+    backgroundColor: "#E8E9Eb",
+    width: 50,
+    height: 50,
     alignItems: 'center',
     justifyContent: 'center'
   },
@@ -129,7 +179,7 @@ const styles = StyleSheet.create({
   },
   elapsedTimeText: {
     fontSize: 10,
-  }
+  },
 });
 
 export default Conversations;
--- features/Messages/Messages.js
+++ features/Messages/Messages.js
@@ -1,50 +1,124 @@
 import React, { Component, useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, Button, Animated, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, Animated, ScrollView, FlatList } from 'react-native';
 import axios from 'axios';
 
-const Messages = ({ currentUser, conversationId }) => {
+const Messages = ({ currentUser, conversation, setConversation }) => {
 
   const [messagesArray, setMessagesArray] = useState([]);
-  const [messageContent, setMessageContent] = useState('');
+
+  const messages = [
+    { id: 1, content: 'Hello sir how are you', author_id: 1 },
+    { id: 2, content: 'Good how are you', author_id: 2 },
+    { id: 3, content: 'Very good.', author_id: 1 },
+    { id: 4, content: 'This is a stimulating conversation.', author_id: 2 },
+    { id: 5, content: 'What comics do you have?', author_id: 1 },
+  ];
 
   useEffect(() => {
-    fetchMessages(currentUser.idToken);
-  }, []);
+    if (!!conversation.conversation_id) {
+      setMessagesArray(messages);
+    } else {
+      setMessagesArray([]);
+    }
+  }, [conversation]);
 
-  const fetchMessages = (idToken) => {
-    axios.get(`/api/messages/${conversationId}`, {
-      headers: {
-        'authorization': IdToken
-      }
-    })
-      .then((result) => {
-        setMessagesArray(result);
-      })
-      .catch((err) => {
-        console.error('Error fetching messages:', err);
-        setMessagesArray([]);
-      });
+
+  // useEffect(() => {
+  //   fetchMessages(currentUser.idToken);
+  // }, []);
+
+  // const fetchMessages = (idToken) => {
+  //   axios.get(`/api/messages/${conversationId}`, {
+  //     headers: {
+  //       'authorization': IdToken
+  //     }
+  //   })
+  //     .then((result) => {
+  //       setMessagesArray(result);
+  //     })
+  //     .catch((err) => {
+  //       console.error('Error fetching messages:', err);
+  //       setMessagesArray([]);
+  //     });
+  // };
+
+  // const postMessage = (messageContent) => {
+  //   axios.post('/api/messages', {
+  //     'conversation_id': conversationId,
+  //     'content': messageContent
+  //   })
+  //     .then((res) => {
+  //       let copy = messagesArray.slice();
+  //       copy.push(res.data);
+  //       setMessagesArray(copy);
+  //     })
+  //     .catch((err) => {
+  //       console.error('Error posting message:', err);
+  //     })
+  //     .finally(() => {
+  //       setMessageContent('');
+  //     });
+  // };
+
+  const renderMessages = ({ item }) => {
+    return (
+      <View style={styles.messageTextContainer}>
+        <Text>
+          {item.content}
+        </Text>
+      </View>
+    );
+
   };
 
-  const postMessage = (messageContent) => {
-    axios.post('/api/messages', {
-      'conversation_id': conversationId,
-      'content': messageContent
-    })
-      .then((res) => {
-        let copy = messagesArray.slice();
-        copy.push(res.data);
-        setMessagesArray(copy);
-      })
-      .catch((err) => {
-        console.error('Error posting message:', err);
-      })
-      .finally(() => {
-        setMessageContent('');
-      });
-  };
+  if (!messagesArray.length) {
+    <View>
+      <Text>Loading...</Text>
+    </View>;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          {conversation.user_1_username}
+        </Text>
+        <View style={styles.horizontalRule} />
+        <View style={styles.messagesContainer}>
+          <FlatList
+            data={messagesArray}
+            renderItem={renderMessages}
+            keyExtractor={(item, index) => index}
+            showsVerticalScrollIndicator={true}
+          />
+        </View>
+      </View>
+    );
+  }
 
-  return (
-    <></>
-  );
 };
+
+const styles = {
+  container: {
+    // alignItems: 'center',
+    // justifyContent: 'flex-start'
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16
+  },
+  horizontalRule: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 16,
+  },
+  messagesContainer: {
+    flexDirection: 'column-reverse',
+    alignItems: 'center',
+    height: '100%'
+  },
+  messageTextContainer: {
+    flex: 1
+  }
+};
+
+export default Messages;

4fe577282bf0b0387c24f8c13469a403bb583ec4
Patrick Nilan <nilan.patrick@gmail.com>
2023-08-02 20:29:41 -0700

    Switched to single quotes

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import HomeMarket from "./features/Marketplace/HomeMarket.js";
-import Signin from "./features/LoginSignup/Signin.js";
+import HomeMarket from './features/Marketplace/HomeMarket.js';
+import Signin from './features/LoginSignup/Signin.js';
 import Cart from './features/Cart/CartPage.js';
 import UserProfile from './features/UserProfile/UserProfile.js';
-import SignIn from "./features/LoginSignup/Signin.js";
+import SignIn from './features/LoginSignup/Signin.js';
 import MyListings from './features/MyListings/MyListings.js';
 import PublicProfile from './features/UserProfile/PublicProfile.js';
 import Purchases from './features/UserProfile/Purchases.js';
@@ -21,27 +21,27 @@ export default function App() {
   const [profile, setProfile] = useState(userData);
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
+      <Tab.Navigator initialRouteName='Home'>
 
-        <Tab.Screen name="Home" component={HomeMarket} />
+        <Tab.Screen name='Home' component={HomeMarket} />
         <Tab.Screen
-          name="Settings"
+          name='Settings'
           options={{
             tabBarLabel: 'Settings',
           }}
           children={() => <SignIn profile={profile} setProfile={setProfile} />}
         />
-        <Tab.Screen name="Cart" component={Cart} />
+        <Tab.Screen name='Cart' component={Cart} />
         <Tab.Screen
-          name="User Profile"
+          name='User Profile'
           options={{
             tabBarLabel: 'User Profile',
           }}
           children={() => <UserProfile profile={profile} setProfile={setProfile} />}
         />
-        <Tab.Screen name="My Listings" component={MyListings} />
-        <Tab.Screen name="Public Profile" component={PublicProfile} />
-        <Tab.Screen name="Purchases" component={Purchases} />
+        <Tab.Screen name='My Listings' component={MyListings} />
+        <Tab.Screen name='Public Profile' component={PublicProfile} />
+        <Tab.Screen name='Purchases' component={Purchases} />
 
       </Tab.Navigator>
     </NavigationContainer>
